dc077dc1861cc4868c967ddfd5a4d09ea2bbf506
Prinz12 <4068095+Prinz12@users.noreply.github.com>
2026-08-02 20:51:34 +0200

    Use GOAT SHA-256 video PIN derivation

--- test/goatCamera.test.js
+++ test/goatCamera.test.js
@@ -104,7 +104,9 @@ describe('goatCamera.js', () => {
         expect(result.iceServers).to.have.length(2);
         expect(result).not.to.have.any.keys('credentials', 'accessKeyId', 'secretAccessKey', 'sessionToken', 'pin');
         const startOptions = axiosStub.get.firstCall.args[1];
-        expect(startOptions.params.pwd).to.equal('825662ee57fae98e3f5b7a2c02b07cac');
+        expect(startOptions.params.pwd).to.equal(
+            'f7e7fbd1a38fe96df90bde06055f81705bfd27f9fa76aaa01143459c9ac42916'
+        );
         expect(startOptions.params.pwd).not.to.equal('9876');
         expect(startOptions.params.did).to.equal(ctx.did);
         expect(startOptions.headers.Authorization).to.equal('Bearer ecovacs-token');
